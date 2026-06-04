360808cb5cc146af413e8171bcc1c6f010894945
eriksandro-barreto <ebarreto.l24@us.edu.cv>
2026-06-04 18:29:33 -0100

    Alguns correções dos erros presentes

--- ProjetoFilaBancario/Fila.java
+++ ProjetoFilaBancario/Fila.java
@@ -1,89 +1,98 @@
+
 public class Fila {
+
     private No inicio;
     private No fim;
     private int tamanho;
+
     public Fila() {
-        inicio = null ;
+        inicio = null;
         fim = null;
         tamanho = 0;
 
         // Adicionar senha
     }
-    public boolean filaVazia(){
-         return inicio == null;
+
+    public boolean filaVazia() {
+        return inicio == null;
     }
-    public void adicionarSenha(Senhas senha){
+
+    public void adicionarSenha(Senhas senha) {
         No novo = new No(senha);
-        if(filaVazia()){
+        if (filaVazia()) {
             inicio = novo;
             fim = novo;
-        }else{
+        } else {
             fim.prox = novo;
             fim = novo;
         }
-         tamanho ++;
-        }
-        //Atender Proximo
-        public Senhas atenderProximo(){
-            if(filaVazia()){
-                return null;
-            }
-            Senhas senha = inicio.senha;
-            senha.MarcarEmAtendimento();
-            inicio = inicio.prox;
+        tamanho++;
+    }
+    //Atender Proximo
 
-            if(inicio == null){
-                fim = null;
-            }
-            tamanho --;
-            return senha;
-        
-        tamanho ++;
+    public Senhas atenderProximo() {
+        if (filaVazia()) {
+            return null;
         }
-        public int verPosiçao(String numeroSenha){
-            int posicao = 1;
-            No auxiliar = inicio;
+        Senhas senha = inicio.senha;
+        senha.MarcarEmAtendimento();
+        inicio = inicio.prox;
 
-            while(auxiliar !=  null){
-                if(auxiliar.senha.getNumeroSenha().equalsIgnoreCase(numeroSenha)){
-                    return posicao;
-                }
-                posicao ++;
-                auxiliar = auxiliar.prox;
-            }
-            return -1;
+        if (inicio == null) {
+            fim = null;
         }
-        //listar filas
-        public String listarFilas(){
-            if(filaVazia()){
-                return "fila vazia";
+        tamanho--;
+        return senha;
 
-            }
-            String texto = "";
-            No auxiliar = inicio;
+    }
 
-            while(auxiliar != null){
-                texto += auxiliar.senha + "\n";
-                auxiliar = auxiliar.prox;
+    public int verPosiçao(String numeroSenha) {
+        int posicao = 1;
+        No auxiliar = inicio;
+
+        while (auxiliar != null) {
+            if (auxiliar.senha.getNumeroSenha().equalsIgnoreCase(numeroSenha)) {
+                return posicao;
             }
-            return texto;
+            posicao++;
+            auxiliar = auxiliar.prox;
         }
-        //Remover Senha
-        public  Senhas remover(){
-            if(filaVazia()){
-                return null;
-            }
-            Senhas senha = inicio.senha;
-            inicio = inicio.senha;
-            if(inicio == null){
-                fim = null;
+        return -1;
+    }
+    //listar filas
+
+    public String listarFilas() {
+        if (filaVazia()) {
+            return "fila vazia";
+
+        }
+        String texto = "";
+        No auxiliar = inicio;
+
+        while (auxiliar != null) {
+            texto += auxiliar.senha + "\n";
+            auxiliar = auxiliar.prox;
+        }
+        return texto;
+    }
+    //Remover Senha
+
+    public Senhas remover() {
+        if (filaVazia()) {
+            return null;
+        }
+        Senhas senha = inicio.senha;
+        inicio = inicio.senha;
+        if (inicio == null) {
+            fim = null;
         }
         return senha;
     }
-    public boolean cancelarSenha(String numeroSenha){
-           if(filaVazia()){
+
+    public boolean cancelarSenha(String numeroSenha) {
+        if (filaVazia()) {
             return false;
-           }
+        }
     }
 
 }
